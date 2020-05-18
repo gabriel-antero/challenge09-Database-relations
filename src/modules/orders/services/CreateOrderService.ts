4eea0bbf5bf42cc5f1c8e3a4b52a6592461fc5ad
Carlos Gabriel <gabrielantero2012@hotmail.com>
2020-05-18 19:01:31 -0300

    all test passed

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,51 @@ interface IRequest {
 @injectable()
 class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not exists');
+    }
+
+    const productsArrayId = products.map(product => {
+      return { id: product.id };
+    });
+
+    const allProducts = await this.productsRepository.findAllById(
+      productsArrayId,
+    );
+
+    const orderProducts = allProducts.map(product => {
+      const singleProduct = products.find(
+        correctProdut => correctProdut.id === product.id,
+      );
+
+      return {
+        product_id: product.id,
+        price: product.price,
+        quantity: singleProduct?.quantity || 0,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,5 +1,7 @@
 import { getRepository, Repository, In } from 'typeorm';
 
+import AppError from '@shared/errors/AppError';
+
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
 import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
@@ -22,20 +24,66 @@ class ProductsRepository implements IProductsRepository {
     quantity,
   }: ICreateProductDTO): Promise<Product> {
     // TODO
+
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({ where: { name } });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const arrayProductsId = products.map(product => product.id);
+
+    const findProducts = await this.ormRepository.find({
+      id: In(arrayProductsId),
+    });
+
+    if (findProducts.length !== arrayProductsId.length) {
+      throw new AppError('This product missing');
+    }
+
+    return findProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const allProducts = await this.findAllById(products);
+
+    const orderProducts = allProducts.map(product => {
+      const oneProduct = products.find(
+        correctProdut => correctProdut.id === product.id,
+      );
+
+      if (!oneProduct) {
+        throw new AppError('Product not found');
+      }
+
+      const newProduct = product;
+
+      if (newProduct.quantity < oneProduct.quantity) {
+        throw new AppError('Insufficient product quantity');
+      }
+
+      newProduct.quantity -= oneProduct.quantity;
+
+      return newProduct;
+    });
+
+    await this.ormRepository.save(orderProducts);
+
+    return orderProducts;
   }
 }
 
